f8908bbeb7192e81d544d29bf9b96189a8a97800
isit422kehs <isit422kehs@gmail.com>
2017-10-01 23:08:05 -0700

    sprint 4 1.2 kristi

---
+++ ISIT422_Product-Notes/ISIT422_Product-Notes/Controllers/NotesController.cs
@@ -0,0 +1,35 @@
+﻿using ISIT422_Product_Notes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ISIT422_Product_Notes.Controllers
+{
+    public class NotesController : ApiController
+    {
+        Note[] notes = new Note[]
+        {
+            new Note {Id = 1, Priority = 3, Subject = "Wake up", Details = "Set alarm of 7:00 am and get out of bed." },
+            new Note {Id = 2, Priority = 2, Subject = "Eat breakfast", Details = "Eat a healthy breakfast."},
+            new Note {Id = 3, Priority = 5, Subject = "Go to work", Details = "Get to work before 9:00 am."}
+        };
+
+        public IEnumerable<Note> GetAllNotes()
+        {
+            return notes;
+        }
+
+        public IHttpActionResult GetNote(int Id)
+        {
+            var note = notes.FirstOrDefault((p) => p.Id == id);
+            if (note==null)
+            {
+                return NotFound();
+            }
+            return Ok(note);
+        }
+    }
+}
