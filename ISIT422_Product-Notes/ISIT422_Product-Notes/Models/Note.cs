2f3b2d8749d1b78c377d20bc0072be7370744cfd
hyejinstar <hyejiniis2@hotmail.com>
2017-10-01 22:26:06 -0700

    hyejin 1.1 added note.cs

---
+++ ISIT422_Product-Notes/ISIT422_Product-Notes/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISIT422_Product_Notes.Models
+{
+    public class Note
+    {
+        int Id { get; set; }
+        string Subject { get; set; }
+        string Details { get; set; }
+        int Priority { get; set; }
+    }
+}
